3dd3fe8de6ad74ccec574e3a59bfe379119f9e52
cedric-san <motovfxsan@gmail.com>
2021-11-03 18:08:57 +0530

    1 file modified

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 const Header = () => {
   return (
     <div className="header">
-      <img src="logo.png" alt="" />
+      <img src="./logo.png" alt="" />
     </div>
   );
 };
